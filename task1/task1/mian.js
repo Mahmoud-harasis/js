f400874dc6e6eb20782b450f811eac99791d7ac4
mahmoud-harasis <mahmoudharasis8@gmail.com>
2022-11-03 15:02:57 +0200

    task

---
+++ task1/task1/mian.js
@@ -0,0 +1,16 @@
+let 
+    x = prompt("Enter X number please :");
+    y = prompt("Enter Y number please :");
+    numX = Number(x);
+    numY = Number(y);
+
+if ( x > y ) {
+    console.log ("Hello World");
+    document.write("Hello World");
+    alert("Hello World");
+}    
+else {
+    console.log ("Goodbye");
+    document.write("Goodbye");
+    alert("Goodbye");
+}
